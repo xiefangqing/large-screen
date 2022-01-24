b9cbb9b94fedcc572d638ff45271fc00f866cacd
xfq <389368595@qq.com>
2022-01-24 13:54:12 +0800

    打架子

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
+const path = require('path')
+const resolve = dir => path.join(__dirname, dir)
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', 'src')
+      .set('components', 'components')
+      .set('utils', 'utils')
+  }
 }
